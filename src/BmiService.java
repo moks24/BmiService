9d38f0b1552077654138cae8e17b66b22d92ad1d
Moks <89726455+moks24@users.noreply.github.com>
2021-11-15 17:56:17 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public float calculate (float bodyMass, float height){
-        float BodyMassIndex = bodyMass/(height * height);
-        return BodyMassIndex;
+        float bodyMassIndex = bodyMass/(height * height);
+        return bodyMassIndex;
     }
 }
